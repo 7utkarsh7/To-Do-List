7de55ffb2de4f022464906beb83e20678212be35
7utkarsh7 <utkarshdohare2@gmail.com>
2020-09-17 01:42:43 +0530

    fixed error

--- app.js
+++ app.js
@@ -161,7 +161,7 @@ app.get("/about", function(req, res) {
   res.render("about");
 });
 let port = process.env.PORT;
-if(port === NULL || port===""){
+if(port === null || port===""){
   port = 3000;
 }
 app.listen( port , function() {
